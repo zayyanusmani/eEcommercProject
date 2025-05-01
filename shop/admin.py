aa9970c65fcf0dad06179857bfed665fecfee258
zayyanusmani <zayyanusmani6@gmail.com>
2025-05-01 16:14:21 +0500

    feat: admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Category, Product, Cart, CartItem, Order, OrderItem
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'category']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at']
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'product', 'quantity']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'status', 'total', 'created_at', 'updated_at']
+    list_filter = ['status', 'created_at']
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'price', 'quantity']
